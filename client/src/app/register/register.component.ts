54af62787e34de06dac0ba89f58df587429b4259
Pratik Patade <pratik.patade@outlook.com>
2025-01-10 19:34:30 +0530

    Angular Routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, output} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -12,6 +13,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   accountService = inject(AccountService);
+  private toaster = inject(ToastrService);
   model: any = {};
   cancelRegister = output<boolean>();
   register() {
@@ -21,7 +23,7 @@ export class RegisterComponent {
         this.cancel();
       },
       error: (error: any) => {
-        console.log(error);
+        this.toaster.error(error.error);
       }
     });
   }
